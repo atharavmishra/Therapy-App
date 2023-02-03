8109586c492cb8ff0ec617e377c0404558c04945
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-02-04 01:15:23 +0530

    Committing project

--- Therapy_App/app/src/main/java/com/example/therapyapp/MainActivity.java
+++ Therapy_App/app/src/main/java/com/example/therapyapp/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.therapyapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.therapyapp.data.remote;
+import com.example.therapyapp.repository.remotedata;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void getrecipe(String query){
+        remotedata.getAPIinterface().getImage2("61a2ba9ae7794bbe914353e4ce889564", true, query).enqueue(new Callback<Root>() {
+            @Override
+            public void onResponse(Call<Root> call, Response<Root> response) {
+
+                arraylist.clear();
+                arraylist.addAll(response.body().getResults());
+                recyclerview.getRecycledViewPool().clear();
+                adapter.notifyDataSetChanged();
+
+                Log.d(TAG, Integer.toString(response.code()));
+                Log.d("myTag", response.toString());
+                Log.d(TAG, Integer.toString(response.code())+(response.raw().request().url()).toString());
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Root> call, Throwable t) {
+
+            }
+        });
+
     }
 }
---
+++ Therapy_App/app/src/main/java/com/example/therapyapp/data/remote.java
@@ -0,0 +1,18 @@
+package com.example.therapyapp.data;
+
+
+
+import com.example.therapyapp.Model.Root;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface remote {
+
+    @GET()
+    Call<Root> getImage(
+
+    );
+
+}
