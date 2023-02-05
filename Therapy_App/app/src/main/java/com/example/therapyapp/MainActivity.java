f5ddaa6a976b343240fc76e52be7e340ca5f3d20
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-02-05 14:24:58 +0530

    Committing project

--- Therapy_App/app/src/main/java/com/example/therapyapp/MainActivity.java
+++ Therapy_App/app/src/main/java/com/example/therapyapp/MainActivity.java
@@ -4,6 +4,8 @@ import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.therapyapp.Model.Datum;
 import com.example.therapyapp.Model.Root;
+import com.example.therapyapp.networkstate.CheckNetwork;
 import com.example.therapyapp.repository.remotedata;
 
 import java.util.ArrayList;
@@ -26,18 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
     Adapter adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerview = findViewById(R.id.recyclerview);
-        recyclerview.setHasFixedSize(true);
-        recyclerview.setLayoutManager(new GridLayoutManager(this,  3));
-        adapter = new Adapter(getApplicationContext(), arraylist);
-        recyclerview.setAdapter(adapter);
+        if(CheckNetwork.isInternetAvailable(this)) //returns true if internet available
+        {
+            setContentView(R.layout.activity_main);
+            recyclerview = findViewById(R.id.recyclerview);
+            recyclerview.setHasFixedSize(true);
+            recyclerview.setLayoutManager(new GridLayoutManager(this, 3));
+            adapter = new Adapter(getApplicationContext(), arraylist);
+            recyclerview.setAdapter(adapter);
+            getImage();
+        } else
+        {
+            Toast.makeText(this,"No Internet Connection", Toast.LENGTH_SHORT).show();
+        }
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        getImage();
+
     }
     private void getImage(){
         remotedata.getAPIinterface().getImage().enqueue(new Callback<Root>() {
@@ -47,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 arraylist.addAll(response.body().getData());
                 recyclerview.getRecycledViewPool().clear();
                 adapter.notifyDataSetChanged();
-
+                findViewById(R.id.progressBar1).setVisibility(View.GONE);
                 Log.d(TAG, Integer.toString(response.code()));
                 Log.d("myTag", response.toString());
                 Log.d(TAG, Integer.toString(response.code())+(response.raw().request().url()).toString());
